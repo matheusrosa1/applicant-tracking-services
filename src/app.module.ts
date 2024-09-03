4ef98e9abf8f5d044cb5beb24850fcc37427bea2
Matheus Rosa <matheusrosataxa@gmail.com>
2024-09-02 22:13:10 -0300

    feat: dockerfile, docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,25 @@ import { JobsModule } from './jobs/jobs.module';
 import { ApplicationsModule } from './applications/applications.module';
 import { InterviewSchedulesModule } from './interview-schedules/interview-schedules.module';
 import { JobAssignmentsModule } from './job-assignments/job-assignments.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+      migrationsRun: true,
+    }),
     CandidatesModule,
     JobsModule,
     ApplicationsModule,
