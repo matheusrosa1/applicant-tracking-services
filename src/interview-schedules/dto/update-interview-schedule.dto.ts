149d1247bd3c671d8308ccc7aecaa612e13bfb07
Matheus Rosa <matheusrosataxa@gmail.com>
2024-09-03 21:01:59 -0300

    reset entidades

--- src/interview-schedules/dto/update-interview-schedule.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInterviewScheduleDto } from './create-interview-schedule.dto';
-
-export class UpdateInterviewScheduleDto extends PartialType(CreateInterviewScheduleDto) {}
